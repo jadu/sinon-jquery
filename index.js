97cb2403ea98399789a28e8e34978782fe00a7db
Daniel Phillimore <daniel.phillimore@jadu.co.uk>
2015-09-11 11:42:43 +0100

    Added entrypoint

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Sinon-jQuery
+ *
+ * Sinon matcher for matching jQuery collections.
+ *
+ * Copyright (c) Jadu
+ *
+ * Released under the MIT license
+ * https://github.com/jadu/sinon-jquery/blob/master/LICENSE.txt
+ */
+
+'use strict';
+
+import Matcher from './src/Matcher';
+import Plugin from './src/Plugin';
+
+export default new Plugin(Matcher);
